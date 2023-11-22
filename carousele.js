54116f0401faa2d567860a187f03cf059ded1b51
Nohara1 <murashko199999@gmail.com>
2023-11-22 13:50:00 +0300

    create: js and start task slider

---
+++ carousele.js
@@ -0,0 +1,11 @@
+let slideOne = document.querySelector('.main__favorite-containe-slider-slide-one');
+let slideTwo = document.querySelector('.main__favorite-containe-slider-slide-two');
+let slideThree = document.querySelector('.main__favorite-containe-slider-slide-three');
+console.log(slideOne);
+
+
+// let array = [slideOne, slideTwo, slideThree];
+
+// slideOne.addEventListener('click', (e) => {
+//     alert('Привет');
+// });
